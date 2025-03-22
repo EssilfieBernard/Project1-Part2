3ea86a64c7fc74ca3179e23d4e503a4ada43627b
Bernard Essilfie <bernard.essilfie@amalitech.com>
2025-03-22 19:31:32 +0000

    update dashboard controller

--- src/main/java/com/s3rds/ImageToS3RDS/DashboardController.java
+++ src/main/java/com/s3rds/ImageToS3RDS/DashboardController.java
@@ -1,9 +1,11 @@
 package com.s3rds.ImageToS3RDS;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
+    @GetMapping
     public String dashboard() {
         return "dashboard";
     }
