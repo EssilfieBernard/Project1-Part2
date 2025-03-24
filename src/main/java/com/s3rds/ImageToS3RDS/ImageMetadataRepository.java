d9104be3b9be568716969cfde7cb563aece93512
Bernard Essilfie <bernard.essilfie@amalitech.com>
2025-03-24 11:07:22 +0000

    remove unused imports and methods

--- src/main/java/com/s3rds/ImageToS3RDS/ImageMetadataRepository.java
+++ src/main/java/com/s3rds/ImageToS3RDS/ImageMetadataRepository.java
@@ -5,10 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ImageMetadataRepository extends JpaRepository<ImageMetadata, Integer> {
     Page<ImageMetadata> findAllByOrderByUploadedAtDesc(Pageable pageable);
-    Optional<ImageMetadata> findByUrl(String url);
 }
